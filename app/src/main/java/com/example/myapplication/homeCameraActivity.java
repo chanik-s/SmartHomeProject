fb8fd9acd00d85e8c898eb623e75461f07043b3a
Chanik <cksdlr3931@gmail.com>
2023-03-30 22:05:44 +0900

    홈캠 기능

--- app/src/main/java/com/example/myapplication/homeCameraActivity.java
+++ app/src/main/java/com/example/myapplication/homeCameraActivity.java
@@ -1,16 +1,90 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class homeCameraActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class homeCameraActivity extends AppCompatActivity  {
+
+    //홈 캠 스트리밍
+    WebView webView;
+    WebSettings webSettings;
+
+    //CCTV ON OFF BTN
+    Button cctvOnButton,cctvOffButton;
+    TextView callText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homecamera);
 
 
+        webView=findViewById(R.id.cctvWeb);
+        callText = (TextView) findViewById(R.id.callText); //신고하기
+        cctvOnButton = (Button) findViewById(R.id.cctvOnButton);
+        cctvOffButton = (Button) findViewById(R.id.cctvOffButton);
+
+        //웹뷰설정내용 ->공부 필요
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadData("<html><head><style type='text/css'>body{margin:auto auto;text-align:center;} " +
+                        "img{width:100%25;} div{overflow: hidden;} </style></head>" +
+                        "<body><div><img src='http://" + "192.168.25.19" + ":8091/?action=stream/'/></div></body></html>",
+                "text/html", "UTF-8");
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    webView.reload();
+                }
+                return true;
+            }
+        }); // WebView 터치 시 새로고침
+
+        callText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(homeCameraActivity.this);
+                builder.setTitle("신고");
+                builder.setMessage("신고하시겠습니까?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:112"));
+                        startActivity(intent);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        }); // 신고하기 버튼 클릭 시 112 전화걸기로 이동
+
+
+
+
+
     }
+
+
 }
